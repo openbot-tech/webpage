b008733aaa7df0cd8f85e38ee588104fea850b96
SvenHolmager <sholmager@gmail.com>
2019-02-15 17:24:39 +0100

    copied working backup over

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Openbotpic from './images/Openbot.svg';
-import twitterpic from './images/twitter.svg';
-import discordpic from './images/discord.svg';
-import githubpic from './images/github.svg';
+import Openbotpic from './images/svg/Openbot.svg';
+import twitterpic from './images/svg/twitter.svg';
+import discordpic from './images/svg/discord.svg';
+import githubpic from './images/svg/github.svg';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 import './App.css';
@@ -16,34 +16,28 @@ class App extends Component {
           <img src={Openbotpic} id="banner" alt="OpenbotBanner"/>   
         </div>
       </div>
-      <p id="text">Join us discussing & developing free, open-source trading algorithms</p>
-      <br />
-      <br />
+      <p id="text">Join us discussing & developing free, open-source trading algorithms.</p>
+      
 
       <Grid>
         <Row className="show-grid">
           <Col xs={6} md={6}>
-            <p><b>Code</b></p>
-            <a className="App-link" href="https://github.com/openbot-tech"
-            > 
-            <img src={githubpic} id="logos" alt="github"/> 
+            <p>Code</p>
+            <a className="App-link" href="https://github.com/openbot-tech">
+              <img src={githubpic} id="logos" alt="github"/> 
             </a>
           </Col>
-
           <Col xs={6} md={6}>
-            <p><b>Chat</b></p>
-            <a className="App-link" href="https://discord.gg/6d4v6YC"
-            > 
-            <img src={discordpic} id="logos" alt="discord"/>  
-            </a> 
-          </Col>
-
-          <Col xs={6} md={6}>
-            <p><b>Follow</b></p>
-            <a className="App-link" href="https://twitter.com/openbot_tech"
-            > 
-            <img src={twitterpic} id="logos" alt="twitter"/>  
+            <p>Chat</p>
+            <a className="App-link" href="https://discord.gg/6d4v6YC"> 
+              <img src={discordpic} id="logos" alt="discord"/>  
             </a>   
+          </Col>
+          <Col xs={6} md={6}>
+            <p>Follow</p>
+            <a className="App-link" href="https://twitter.com/openbot_tech"> 
+            <img src={twitterpic} id="logos" alt="twitter"/>  
+            </a> 
           </Col>
         </Row>
       </Grid>
