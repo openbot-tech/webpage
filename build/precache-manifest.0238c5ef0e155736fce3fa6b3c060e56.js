407f8352b5f9afe40837a1bbfda759d96f15577a
seichris <cerujurasa@gmail.com>
2019-03-10 12:28:54 +0100

    try to deploy gh-page

---
+++ build/precache-manifest.0238c5ef0e155736fce3fa6b3c060e56.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12686b6a739bddd0c1d",
+    "url": "/static/css/main.19730de5.chunk.css"
+  },
+  {
+    "revision": "b12686b6a739bddd0c1d",
+    "url": "/static/js/main.b12686b6.chunk.js"
+  },
+  {
+    "revision": "a829dcbb0026884e5f01",
+    "url": "/static/js/1.a829dcbb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8208c903749022cd5d2539347f2405e",
+    "url": "/static/media/Openbot_black.e8208c90.svg"
+  },
+  {
+    "revision": "f10eba06c208b7de4b405483c8c9c6d5",
+    "url": "/static/media/discord.f10eba06.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "229b813a172a924d25b38ded63e81972",
+    "url": "/static/media/stack.229b813a.svg"
+  },
+  {
+    "revision": "3cb44f0a1807d20fa90c1702afe1f8d8",
+    "url": "/static/media/test.3cb44f0a.svg"
+  },
+  {
+    "revision": "cb59220a7d528bff50ee333cc89bffca",
+    "url": "/static/media/traders.cb59220a.png"
+  },
+  {
+    "revision": "04b00104328994d513545a9133ee183c",
+    "url": "/static/media/developers.04b00104.png"
+  },
+  {
+    "revision": "0c30ca1f127d8ab7e9339850f5bda461",
+    "url": "/static/media/tradingscreen.0c30ca1f.png"
+  },
+  {
+    "revision": "28ca8de76f69df47bb2579c78188494a",
+    "url": "/static/media/twitter.28ca8de7.svg"
+  },
+  {
+    "revision": "750111978a58cf3f323393c7407bb963",
+    "url": "/index.html"
+  }
+];
